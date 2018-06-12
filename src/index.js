72a8dc9fa0adc6f8784de6afe86e87ae8a0711fe
Mohit Bhansali <mohitbhansali11@gmail.com>
2018-06-12 14:21:07 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import registerServiceWorker from './registerServiceWorker';
 let musicProvider = MusicProvider.sharedProvider();
 musicProvider.configure();
 let musicInstance = musicProvider.getMusicInstance();
-console.log("Music Instance", musicInstance);
 
 ReactDOM.render(<App musicInstance={musicInstance} />, document.getElementById('root'));
 registerServiceWorker();
